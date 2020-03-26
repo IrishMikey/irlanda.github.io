ea168308e6378791344a9aae61d0bddc30abab72
IrishMikey <47180065+IrishMikey@users.noreply.github.com>
2020-03-26 13:35:01 +0000

    Upload of files

---
+++ JavaScript/TestJS.js
@@ -0,0 +1,24 @@
+//Get the button:
+const buttonToTop = document.getElementById("BtnTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+lepButtonImg.addEventListener("click", function(){
+	menu.style.animation = "menuDrop 5.7s ease-in-out 1"; 
+});
